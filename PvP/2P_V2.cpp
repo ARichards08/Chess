971b781913fce5f64f270f8b061daf7360ef67c2
ARichards08 <88149323+ARichards08@users.noreply.github.com>
2024-01-15 11:37:34 +0000

    Got the board setup and the print working, might just need to have a function to sort what the numbers are into letters. Afterwards, start on move function, don't worry about legal moves yet

--- PvP/2P_V2.cpp
+++ PvP/2P_V2.cpp
@@ -2,17 +2,76 @@
 #include <iostream>
 #include <string>
 #include <vector>
-//#include <unordered_map>
 
 // Enumerations
 
+
+
+
+// Namespaces
+namespace Piece{
+
+    // Pieces are given an int value, 0 for none, 1-6 for the black pieces and 7-12 for the white pieces
+    // Figure Enumeration
+    enum Figure{None, Pawn, Rook, Knight, Bishop, Queen, King};
+
+    // Colour Enumeration
+    enum Colour{Black, White};
+
+    // Add a piece function, returns the piece's int value
+    unsigned short int Add(Colour Col, Figure Fig){
+        return Col*6+Fig;
+    };
+};
+
 // Classes
+
+
 
 // Move notation, if two same pieces can move to the same position, the file is added before the end position. If they are in the same file, the rank is added instead
 // Rd8 > Rdd8 or R3d8 for moving a rook
 
 // Functions
 
+// Function to setup the chess board at the start of the program
+void Setup_Board(std::vector<unsigned short int> &Board){
+    // Add Row 1, White non-pawns
+    Board[0]=Piece::Add(Piece::White, Piece::Rook), Board[1]=Piece::Add(Piece::White, Piece::Knight), Board[2]=Piece::Add(Piece::White, Piece::Bishop), Board[3]=Piece::Add(Piece::White, Piece::Queen), Board[4]=Piece::Add(Piece::White, Piece::King), Board[5]=Piece::Add(Piece::White, Piece::Bishop), Board[6]=Piece::Add(Piece::White, Piece::Knight), Board[7]=Piece::Add(Piece::White, Piece::Rook);
+    // Add Row 2, White pawns
+    for (int i=8*1; i<8*2; i++){
+        Board[i]=Piece::Add(Piece::White, Piece::Pawn);
+    };
+    // Add Row 7, Black Pawns
+    for (int i=8*6; i<8*7; i++){
+        Board[i]=Piece::Add(Piece::Black, Piece::Pawn);
+    };
+    // Add Row 8, Black non-pawns
+    Board[56]=Piece::Add(Piece::Black, Piece::Rook), Board[57]=Piece::Add(Piece::Black, Piece::Knight), Board[58]=Piece::Add(Piece::Black, Piece::Bishop), Board[59]=Piece::Add(Piece::Black, Piece::Queen), Board[60]=Piece::Add(Piece::Black, Piece::King), Board[61]=Piece::Add(Piece::Black, Piece::Bishop), Board[62]=Piece::Add(Piece::Black, Piece::Knight), Board[63]=Piece::Add(Piece::Black, Piece::Rook);
+
+};
+
+// Function to print the board
+void Print_Board(std::vector<unsigned short int> &Board){
+    int k;
+
+    // Files
+    std::cout << "    a  b  c  d  e  f  g  h \n" << std::endl;
+
+    for (int i=7; i>=0; i--){
+        // Numbers
+        std::cout << i+1 << "  ";
+
+        for (int j=0; j<8; j++) {
+            k=i*8+j;
+            if (Board[k]<10){
+                std::cout << " " << Board[k] << " ";
+            } else {
+            std::cout << Board[k] << " ";
+            };
+        };
+        std::cout << std::endl;
+    };
+};
 
 // Program
 
@@ -21,7 +80,7 @@ int main (int argc, char *argv[]){
 // Board
 std::vector<unsigned short int> Board(64, 0);
 
-
-
+Setup_Board(Board);
+Print_Board(Board);
 
 }
